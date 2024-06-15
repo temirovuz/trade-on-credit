3ffdef4f3adb1b3e9a0bada9da566dbfac399093
temirovuz <temirov-muhammad@mail.ru>
2024-06-15 16:35:21 +0500

    update

--- auth/auth.py
+++ auth/auth.py
@@ -8,10 +8,13 @@ from auth.schema import Token, CreateShop
 from core.database import get_db
 
 router = APIRouter()
+
+
 @router.post('signup', status_code=201)
 def create_shop_(user_data: CreateShop, db: Session = Depends(get_db)):
     shop = create_shop(user_data.name, user_data.phone, user_data.password, db)
     return shop
+
 
 @router.post('/login', status_code=200, response_model=Token)
 def login():
